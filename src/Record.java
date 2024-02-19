2fa3210729676f7cc203bb7a2c9d9f7cf067eeec
AdamLamy <110918238+AdamLamy@users.noreply.github.com>
2024-02-19 22:40:08 +0000

    Add files via upload

---
+++ src/Record.java
@@ -0,0 +1,90 @@
+public class Record {
+    private String recordId;
+    private String customerId;
+    private String loanType;
+    private float interest;
+    private int amountToPay;
+    private int loanTerm;
+
+    // Default record
+    public Record() {
+        this.recordId = "000000";
+        this.customerId = "AAA000";
+        this.loanType = "Other";
+        this.interest = 0;
+        this.amountToPay = 0;
+        this.loanTerm = 0;
+    }
+
+    // Custom record
+    public Record(String recordId, String customerId, String loanType, int interest, int amountToPay, int loanTerm) {
+        this.recordId = recordId;
+        this.customerId = customerId;
+        this.loanType = loanType;
+        this.interest = interest;
+        this.amountToPay = amountToPay;
+        this.loanTerm = loanTerm;
+    }
+
+    // Converts object's information to a string for output
+    public String toString() {
+        return "LoanRecord{" +
+                "recordId='" + recordId + '\'' +
+                ", customerId='" + customerId + '\'' +
+                ", loanType='" + loanType + '\'' +
+                ", interest=" + interest +
+                ", amountToPay=" + amountToPay +
+                ", loanTerm=" + loanTerm +
+                '}';
+    }
+
+
+    // Getters and setters for each attribute
+    public String getRecordId() {
+        return recordId;
+    }
+
+    public void setRecordId(String recordId) {
+        this.recordId = recordId;
+    }
+
+    public String getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(String customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getLoanType() {
+        return loanType;
+    }
+
+    public void setLoanType(String loanType) {
+        this.loanType = loanType;
+    }
+
+    public float getInterest() {
+        return interest;
+    }
+
+    public void setInterest(float interest) {
+        this.interest = interest;
+    }
+
+    public int getAmountToPay() {
+        return amountToPay;
+    }
+
+    public void setAmountToPay(int amountToPay) {
+        this.amountToPay = amountToPay;
+    }
+
+    public int getLoanTerm() {
+        return loanTerm;
+    }
+
+    public void setLoanTerm(int yearTerm) {
+        this.loanTerm = yearTerm;
+    }
+}
---
+++ src/XYZBank.java
@@ -0,0 +1,76 @@
+import java.util.List;
+import java.util.Objects;
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class XYZBank {
+    public static void main(String[] args) {
+        Scanner inputScanner = new Scanner(System.in);
+        List<Record> records = new ArrayList<>();
+
+        System.out.print("\nEnter maximum records: ");
+        int maxRecords = inputScanner.nextInt();
+
+        // Creates 'maxRecords' number of records of which the user enters the data for
+        for(int i = 0; i < maxRecords; i++){
+            System.out.print("\nEnter Record ID: ");
+            String recordId = inputScanner.next();
+
+            System.out.print("Enter Customer ID: ");
+            String customerId = inputScanner.next();
+
+            System.out.print("Enter Loan Type: ");
+            String loanType = inputScanner.next();
+
+            System.out.print("Enter Interest: ");
+            int interest = inputScanner.nextInt();
+
+            System.out.print("Enter amount left to pay: ");
+            int amountToPay = inputScanner.nextInt();
+
+            System.out.print("Enter Loan Term: ");
+            int loanTerm = inputScanner.nextInt();
+
+            Record newRecord = new Record(recordId, customerId, loanType, interest, amountToPay, loanTerm);
+            records.add(newRecord);
+
+
+            if(i < maxRecords - 1){
+                String choice = getChoice(inputScanner);
+                if (Objects.equals(choice.toUpperCase(), "N")) {
+                    break;
+                } else if (!choice.equalsIgnoreCase("Y")){
+                    System.out.println("Invalid answer.");
+                    getChoice(inputScanner);
+                }
+            }
+
+        }
+
+        System.out.println("\n++++++++++++++++++++++++++\n    Outputting Records\n++++++++++++++++++++++++++\n");
+        System.out.println("Maximum records: " + maxRecords);
+        System.out.println("Registered records: " + records.size() + "\n");
+
+        // table header
+        System.out.printf("%-15s %-15s %-15s %-10s %-15s %-10s%n",
+                "Record ID", "Customer ID", "Loan Type", "Interest", "Amount Left", "Loan Term");
+
+        // output record data
+        for (Record record : records) {
+            System.out.printf("%-15s %-15s %-15s %-10s %-15s %-10s%n",
+                    record.getRecordId(),
+                    record.getCustomerId(),
+                    record.getLoanType(),
+                    record.getInterest() + "%",
+                    "£" + (record.getAmountToPay() * 10000),
+                    record.getLoanTerm() + " years");
+        }
+    }
+
+    // used to decide whether to ask the user for inputs for a new record
+    private static String getChoice(Scanner inputScanner) {
+        System.out.print("Enter new record?  (Y/N)  ");
+        return inputScanner.next();
+    }
+
+}
